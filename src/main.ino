2d768adde901b21f08e126824f69b69edb51f09a
Alasdair Penman <alasdair.penman@hotmail.com>
2019-07-06 08:34:39 +0000

    Calibrated stepper motors for correct belt movements and introduced stop() function and finer state control of belt

--- src/main.ino
+++ src/main.ino
@@ -17,7 +17,9 @@ AccelStepper frontStepper(HALFSTEP, frontMotorPin1, frontMotorPin3, frontMotorPi
 AccelStepper rearStepper(HALFSTEP, rearMotorPin1, rearMotorPin3, rearMotorPin2, rearMotorPin4);
 
 bool beltActive = false;
-bool _test = true;
+bool firstRun = false;
+bool finalRun = false;
+bool _test = false;
 
 void setup() {
 	Serial.begin(9600);
@@ -25,7 +27,7 @@ void setup() {
 }
 
 void loop() {
-	float distance = 600;
+	float distance = 5600;
 	if(_test) run();
 	else {
 		if (Serial.available()) {
@@ -33,21 +35,35 @@ void loop() {
 			Serial.write(data);
 			if(data == 's' && !beltActive && frontStepper.distanceToGo() == 0 && rearStepper.distanceToGo() == 0) {
 				beltActive = true;
+				firstRun = true;
 				towardsDesk(distance);
 				run();
 
 				Serial.print("AA");
 			}
 		} else {
-			if(!beltActive) Serial.write('N');
+			if(!firstRun) Serial.write('N');
 		}
-		if(beltActive) {
-			if (frontStepper.distanceToGo() == 0 && rearStepper.distanceToGo() == 0) {
-				towardsCupboard(distance - 100);
-				beltActive = false;
+		if(firstRun) {
+			if (rearStepper.distanceToGo() == 0) { //Only interested in the rear stepper reaching its target
+				towardsCupboard(1);
+				run();
+				firstRun = false;
+				finalRun = true;
 				Serial.print("D");
 			} else {
 				Serial.print("M");
+			}
+		}
+		if(finalRun) {
+			if (frontStepper.distanceToGo() == 0) { //Only interested in the front stepper reaches its target
+				firstRun = false;
+				finalRun = false;
+				beltActive = false;
+
+				//Deactivate belt
+				stop();
+				run();
 			}
 		}
 		run();
@@ -55,7 +71,8 @@ void loop() {
 }
 
 void test() {
-	towardsDesk(1000);
+	//towardsDesk(4200);
+	//towardsCupboard(4200);
 }
 
 void frontToCupboard() {
@@ -83,30 +100,56 @@ void lock() {
 
         rearStepper.moveTo(500);
 }
-float speed = 75.0;
-float acceleration = 75.0;
-void towardsCupboard(int dist) {
-        frontStepper.setMaxSpeed(speed);
-        frontStepper.setAcceleration(acceleration);
-        frontStepper.setSpeed(speed);
 
-        rearStepper.setMaxSpeed(speed);
-        rearStepper.setAcceleration(acceleration);
-        rearStepper.setSpeed(speed);
+void towardsCupboard(int _dist) {
+	//Driver
+	float _fSpeed = 55.0;
+	float _fAccel = 55.0;
+	//Back
+	float _rSpeed = 80.0;
+	float _rAccel = 80.0;
 
-        frontStepper.moveTo(dist + 100);
-        rearStepper.moveTo(-dist);
+        frontStepper.setMaxSpeed(_fSpeed);
+        frontStepper.setAcceleration(_fAccel);
+        frontStepper.setSpeed(_fSpeed);
+
+        rearStepper.setMaxSpeed(_rSpeed);
+        rearStepper.setAcceleration(_rAccel);
+        rearStepper.setSpeed(_rSpeed);
+
+        frontStepper.moveTo(_dist);
+        rearStepper.moveTo(-_dist);
 }
 
 void towardsDesk(int dist) {
-        frontStepper.setMaxSpeed(speed);
-        frontStepper.setAcceleration(acceleration);
-        frontStepper.setSpeed(speed);
+	//Back
+	float fSpeed = 50.0;
+	float fAccel = 50.0;
+	//Driver
+	float rSpeed = 75.0;
+	float rAccel = 75.0;
 
-        rearStepper.setMaxSpeed(speed);
-        rearStepper.setAcceleration(acceleration);
-        rearStepper.setSpeed(speed);
+        frontStepper.setMaxSpeed(fSpeed);
+        frontStepper.setAcceleration(fAccel);
+        frontStepper.setSpeed(fSpeed);
+
+        rearStepper.setMaxSpeed(rSpeed);
+        rearStepper.setAcceleration(rAccel);
+        rearStepper.setSpeed(rSpeed);
 
         frontStepper.moveTo(-dist);
-        rearStepper.moveTo(dist + 99);
+        rearStepper.moveTo(dist);
+}
+
+void stop() {
+        frontStepper.setMaxSpeed(0);
+        frontStepper.setAcceleration(0);
+        frontStepper.setSpeed(0);
+
+        rearStepper.setMaxSpeed(50);
+        rearStepper.setAcceleration(50);
+        rearStepper.setSpeed(50);
+
+        frontStepper.moveTo(0);
+        rearStepper.moveTo(100);
 }
